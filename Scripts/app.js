40ba370b632e451a06fb6d11e44285145696adb4
Dave Gwyn <gwyn@dgfprofessionalservices.com>
2019-01-13 08:48:15 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// self-excuting anonymous function
+// IIFE - Immediately Invoked Function Expression    
+(function() {
+    let myFunctionalVariable = 0;
+
+    let Start = function() {
+        let myLocalVariable = 0;
+        console.log(`App Started... ${myFunctionalVariable}`);
+    }
+
+    window.addEventListener("load", Start);
+})();
